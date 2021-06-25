6d19ce72f843f66338c29a623454cf838a6ec376
Danielle <78396825+barnhart0120@users.noreply.github.com>
2021-06-24 17:15:23 -0700

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-function App() {
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function App() {
   return (
     <div className="App">
-      <footer>
-        <a href="https://github.com/barnhart0120/react-weather-app-final">
-          open-source code
-        </a>{" "}
-        by Danielle Barnhart
-      </footer>
+      <div className="container">
+        <footer>
+          <a href="https://github.com/barnhart0120/react-weather-app-final">
+            open-source code
+          </a>{" "}
+          by Danielle Barnhart
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
